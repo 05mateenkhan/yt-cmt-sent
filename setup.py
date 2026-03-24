515885e4f930da079d37300b703763e5664e5c7b
mateenkhan <05mateenkhan@gmail.com>
2026-03-24 11:30:52 +0530

    ini commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='yt cmt sent ana',
+    author='MateenKhan',
+    license='',
+)
