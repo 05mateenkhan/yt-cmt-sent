3dd47c9551db2c53e52ceeecbc8bdc176a5b80c8
mateenkhan <05mateenkhan@gmail.com>
2026-03-26 08:38:31 +0530

    add model load test

---
+++ scripts/test_load_model.py
@@ -0,0 +1,37 @@
+import mlflow.pyfunc
+import pytest
+from mlflow.tracking import MlflowClient
+import os
+# Set your remote tracking URI
+from dotenv import load_dotenv
+load_dotenv()
+
+os.environ["MLFLOW_TRACKING_USERNAME"] = os.getenv("DAGSHUB_TOKEN")
+os.environ["MLFLOW_TRACKING_PASSWORD"] = os.getenv("DAGSHUB_TOKEN")
+
+mlflow.set_tracking_uri("https://dagshub.com/05mateenkhan/comments-analyzer.mlflow")
+
+
+
+@pytest.mark.parametrize("model_name, stage", [
+    ("yt_chrome_plugin_model", "staging"),])
+def test_load_latest_staging_model(model_name, stage):
+    client = MlflowClient()
+    
+    # Get the latest version in the specified stage
+    latest_version_info = client.get_latest_versions(model_name, stages=[stage])
+    latest_version = latest_version_info[0].version if latest_version_info else None
+    
+    assert latest_version is not None, f"No model found in the '{stage}' stage for '{model_name}'"
+
+    try:
+        # Load the latest version of the model
+        model_uri = f"models:/{model_name}/{latest_version}"
+        model = mlflow.pyfunc.load_model(model_uri)
+
+        # Ensure the model loads successfully
+        assert model is not None, "Model failed to load"
+        print(f"Model '{model_name}' version {latest_version} loaded successfully from '{stage}' stage.")
+
+    except Exception as e:
+        pytest.fail(f"Model loading failed with error: {e}")
